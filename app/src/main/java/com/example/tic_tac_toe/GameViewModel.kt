31a62d7c9865a15a51f24e1a25c83665b8aed97c
Nicola Masarone <nick67.android@gmail.com>
2021-07-25 12:52:43 +0200

    Translation resources added. Winner lines added (vertical and horizontal, to do diagonals).

--- app/src/main/java/com/example/tic_tac_toe/GameViewModel.kt
+++ app/src/main/java/com/example/tic_tac_toe/GameViewModel.kt
@@ -6,13 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 const val emptyCell = ' '
-const val xIsPlaying = "X is playing"
-const val oIsPlaying = "O is playing"
-const val fullCell = "Cell is full!"
-const val xWins = "X wins!"
-const val oWins = "O wins!"
-const val drawGame = "Draw"
-const val resetMsg = "Long click on this message to reset game"
 
 class GameViewModel: ViewModel() {
 
@@ -21,8 +14,28 @@ class GameViewModel: ViewModel() {
     }
     private var _grid = MutableLiveData(gameGrid)!!
     val grid: LiveData<Array<Array<Char>>> = _grid
-    private var _helpMessage = MutableLiveData<String>(xIsPlaying)
+
+    private var _helpMessage = MutableLiveData<String>(MainActivity.instance.getString(R.string.x_is_playing))
     val helpMessage: MutableLiveData<String> = _helpMessage
+
+    private var _winVertLine1Vis = MutableLiveData<Boolean>(false)
+    val winVertLine1Vis: LiveData<Boolean> = _winVertLine1Vis
+    private var _winVertLine2Vis = MutableLiveData<Boolean>(false)
+    val winVertLine2Vis: LiveData<Boolean> = _winVertLine2Vis
+    private var _winVertLine3Vis = MutableLiveData<Boolean>(false)
+    val winVertLine3Vis: LiveData<Boolean> = _winVertLine3Vis
+    private var _winHorLine1Vis = MutableLiveData<Boolean>(false)
+    val winHorLine1Vis: LiveData<Boolean> = _winHorLine1Vis
+    private var _winHorLine2Vis = MutableLiveData<Boolean>(false)
+    val winHorLine2Vis: LiveData<Boolean> = _winHorLine2Vis
+    private var _winHorLine3Vis = MutableLiveData<Boolean>(false)
+    val winHorLine3Vis: LiveData<Boolean> = _winHorLine3Vis
+    private var _winDiagLine1Vis = MutableLiveData<Boolean>(false)
+    val winDiagLine1Vis: LiveData<Boolean> = _winDiagLine1Vis
+    private var _winDiagLine2Vis = MutableLiveData<Boolean>(false)
+    val winDiagLine2Vis: LiveData<Boolean> = _winDiagLine2Vis
+    private var winnerLine: MutableLiveData<Boolean> = _winVertLine1Vis
+
 
     private var playingSymbol = 'X'
     private var gameOver = false
@@ -35,15 +48,15 @@ class GameViewModel: ViewModel() {
                 _helpMessage.value = when {
                     symbWins(gameGrid, 'X') -> {
                         gameOver = true
-                        xWins + "\n" + resetMsg
+                        MainActivity.instance.getString(R.string.x_wins) + "\n" + MainActivity.instance.getString(R.string.reset_message)
                     }
                     symbWins(gameGrid, 'O') -> {
                         gameOver = true
-                        oWins + "\n" + resetMsg
+                        MainActivity.instance.getString(R.string.o_wins) + "\n" + MainActivity.instance.getString(R.string.reset_message)
                     }
                     countSymb(gameGrid, emptyCell) == 0 -> {
                         gameOver = true
-                        drawGame + "\n" + resetMsg
+                        MainActivity.instance.getString(R.string.draw) + "\n" + MainActivity.instance.getString(R.string.reset_message)
                     }
                     else -> {
                         playingSymbol = if (playingSymbol == 'X') {
@@ -52,27 +65,62 @@ class GameViewModel: ViewModel() {
                             'X'
                         }
                         if (playingSymbol == 'X') {
-                            xIsPlaying
+                            MainActivity.instance.getString(R.string.x_is_playing)
                         } else {
-                            oIsPlaying
+                            MainActivity.instance.getString(R.string.o_is_playing)
                         }
                     }
                 }
             } else {
-                _helpMessage.value = _helpMessage.value + "\n" + fullCell
+                _helpMessage.value = _helpMessage.value + "\n" + MainActivity.instance.getString(R.string.full_cell)
             }
         }
     }
 
     private fun symbWins(myGrid: Array<Array<Char>>, symb: Char):Boolean {
-        return  myGrid[0][0] == symb && myGrid[0][1] == symb && myGrid[0][2] == symb ||
-                myGrid[1][0] == symb && myGrid[1][1] == symb && myGrid[1][2] == symb ||
-                myGrid[2][0] == symb && myGrid[2][1] == symb && myGrid[2][2] == symb ||
-                myGrid[0][0] == symb && myGrid[1][0] == symb && myGrid[2][0] == symb ||
-                myGrid[0][1] == symb && myGrid[1][1] == symb && myGrid[2][1] == symb ||
-                myGrid[0][2] == symb && myGrid[1][2] == symb && myGrid[2][2] == symb ||
-                myGrid[0][0] == symb && myGrid[1][1] == symb && myGrid[2][2] == symb ||
-                myGrid[0][2] == symb && myGrid[1][1] == symb && myGrid[2][0] == symb
+        return when {
+            myGrid[0][0] == symb && myGrid[0][1] == symb && myGrid[0][2] == symb -> {
+                _winHorLine1Vis.value = true
+                winnerLine = _winHorLine1Vis
+                true
+            }
+            myGrid[1][0] == symb && myGrid[1][1] == symb && myGrid[1][2] == symb -> {
+                _winHorLine2Vis.value = true
+                winnerLine = _winHorLine2Vis
+                true
+            }
+            myGrid[2][0] == symb && myGrid[2][1] == symb && myGrid[2][2] == symb -> {
+                _winHorLine3Vis.value = true
+                winnerLine = _winHorLine3Vis
+                true
+            }
+            myGrid[0][0] == symb && myGrid[1][0] == symb && myGrid[2][0] == symb -> {
+                _winVertLine1Vis.value = true
+                winnerLine = _winVertLine1Vis
+                true
+            }
+            myGrid[0][1] == symb && myGrid[1][1] == symb && myGrid[2][1] == symb -> {
+                _winVertLine2Vis.value = true
+                winnerLine = _winVertLine2Vis
+                true
+            }
+            myGrid[0][2] == symb && myGrid[1][2] == symb && myGrid[2][2] == symb -> {
+                _winVertLine3Vis.value = true
+                winnerLine = _winVertLine3Vis
+                true
+            }
+            myGrid[0][0] == symb && myGrid[1][1] == symb && myGrid[2][2] == symb -> {
+                _winDiagLine1Vis.value = true
+                winnerLine = _winDiagLine1Vis
+                true
+            }
+            myGrid[0][2] == symb && myGrid[1][1] == symb && myGrid[2][0] == symb -> {
+                _winDiagLine2Vis.value = true
+                winnerLine = _winDiagLine2Vis
+                true
+            }
+                    else -> false
+        }
     }
 
     private fun countSymb(myGrid: Array<Array<Char>>, symb: Char): Int {
@@ -93,8 +141,9 @@ class GameViewModel: ViewModel() {
                 }
             }
             _grid.postValue(gameGrid)
+            winnerLine.value = false
             playingSymbol = 'X'
-            _helpMessage.value = xIsPlaying
+            _helpMessage.value = MainActivity.instance.getString(R.string.x_is_playing)
             gameOver = false
         }
         return false
--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -7,13 +7,19 @@ import androidx.databinding.DataBindingUtil
 import com.example.tic_tac_toe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var instance: AppCompatActivity
+            private set
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        instance = this
 
         val gameViewModel: GameViewModel by viewModels()
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.layoutGameViewModel = gameViewModel
         binding.lifecycleOwner = this
-
     }
 }
